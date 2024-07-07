2a9919d6fb759913a0f8764ac4e8de49cf74d01c
reqres404 <adittyapatil78@gmail.com>
2024-07-08 00:01:53 +0530

    -tagline changes

--- Client/src/Pages/News/News.jsx
+++ Client/src/Pages/News/News.jsx
@@ -13,8 +13,8 @@ const News = () => {
   return (
     <div className="app-container">
       <h1 className="tagline">
-      Stay Informed, Stay Engaged With
-      <br />Instant News Summaries
+      Stay <span className="highlight">Informed</span>, Stay <span className="highlight">Engage</span> With
+      <br />Instant News <span className="highlight">Summaries</span>
       {/* News at Lightning <span className="highlight">Speed⚡</span> */}
       </h1>
       <Filter onCategoryChange={handleCategoryChange} />
